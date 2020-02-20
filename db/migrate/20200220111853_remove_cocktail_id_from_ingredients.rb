c4c9e0ec5794ab3a05b09ef2ee71f3395c6aebe6
Katrina Au <katrinaau95@gmail.com>
2020-02-20 17:13:53 +0000

    Cocktail page completed

---
+++ db/migrate/20200220111853_remove_cocktail_id_from_ingredients.rb
@@ -0,0 +1,5 @@
+class RemoveCocktailIdFromIngredients < ActiveRecord::Migration[6.0]
+  def change
+    remove_reference :ingredients, :cocktail, index: true, foreign_key: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning the database...'
+
+puts 'Database cleaned!'
+puts 'Adding ingredients...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file = open(url).read
+result = JSON.parse(file)
+
+result["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+puts 'Success! Ingredients added.'
